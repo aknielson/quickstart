f35be643494d64bd4787175bef4478241378e758
Allen Nielson <aknielson@hotmail.com>
2019-02-05 15:15:58 -0800

    Adding User Authentication with OpenID Connect

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -4,6 +4,8 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4;
 using IdentityServer4.Test;
 
 namespace IdentityServer
@@ -12,9 +14,10 @@ namespace IdentityServer
   {
     public static IEnumerable<IdentityResource> GetIdentityResources()
     {
-      return new IdentityResource[]
+      return new List<IdentityResource>
       {
-        new IdentityResources.OpenId()
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
       };
     }
 
@@ -44,7 +47,7 @@ namespace IdentityServer
           },
 
           // scopes that client has access to
-          AllowedScopes = { "api1" }
+          AllowedScopes = {"api1"}
         },
         new Client
         {
@@ -55,7 +58,28 @@ namespace IdentityServer
           {
             new Secret("secret".Sha256())
           },
-          AllowedScopes = { "api1" }
+          AllowedScopes = {"api1"}
+        },
+        // other clients omitted...
+
+        // OpenID Connect implicit flow client (MVC)
+        new Client
+        {
+          ClientId = "mvc",
+          ClientName = "MVC Client",
+          AllowedGrantTypes = GrantTypes.Implicit,
+
+          // where to redirect to after login
+          RedirectUris = {"http://localhost:5002/signin-oidc"},
+
+          // where to redirect to after logout
+          PostLogoutRedirectUris = {"http://localhost:5002/signout-callback-oidc"},
+
+          AllowedScopes = new List<string>
+          {
+            IdentityServerConstants.StandardScopes.OpenId,
+            IdentityServerConstants.StandardScopes.Profile
+          }
         }
       };
     }
@@ -68,13 +92,25 @@ namespace IdentityServer
         {
           SubjectId = "1",
           Username = "alice",
-          Password = "password"
+          Password = "password",
+
+          Claims = new []
+          {
+            new Claim("name", "Alice"),
+            new Claim("website", "https://alice.com")
+          }
         },
         new TestUser
         {
           SubjectId = "2",
           Username = "bob",
-          Password = "password"
+          Password = "password",
+
+          Claims = new []
+          {
+            new Claim("name", "Bob"),
+            new Claim("website", "https://bob.com")
+          }
         }
       };
     }
